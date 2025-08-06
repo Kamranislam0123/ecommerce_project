3bfd3cbd9a07e96f80f24bad831878ea2c8ca428
Kamranislam0123 <kamranislam8@gmail.com>
2025-08-06 17:39:59 +0600

    🐛 fix(product): resolve size and color relation issues
    - fix size and color relation issues
    - resolve database column name issues

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable =['code','name','slug', 'category_id','sub_category_id','price','image','thum_image','discount','product_size','color','short_details','description','is_featured','is_offer','status','save_by','update_by','ip_address'];
+    protected $fillable =['code','name','slug', 'category_id','sub_category_id','price','image','thum_image','discount','size_id','color_id','product_size','color','short_details','description','is_featured','is_offer','status','save_by','update_by','ip_address'];
      
     public function productImage(){
         return $this->hasMany(ProductImage::class);
